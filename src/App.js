267dac3ff32bbe4ad0fd7f9b6ef62a9a52b5eadd
panda <p4nd4@posteo.de>
2024-01-13 15:09:50 +0100

    init useReduce for speed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import "./App.css";
 import Car from "./components/Car";
+import { useReducer } from "react";
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case "accelerate":{
+      console.log('acc', state.speed);
+      return { ...state, speed: state.speed +5}
+    }
+    case "break":{
+      return { ...state, speed: state.speed -5}
+    }
+    // better way to handle default?
+    default: {
+      return state;
+    }
+  }
+}
 
 function App() {
+  const initialState = {started: true, speed: 0};
+  const [carState, dispatch]= useReducer(reducer, initialState);
+
+  const accelerate = () => {
+    if (initialState.started === true) {
+      dispatch({type: "accelerate"});
+      console.log('Current state after accelerate:', carState);
+    }
+  };
+  const brake = () => {
+    if (initialState.speed >0) {
+      dispatch({type: "accelerate"});
+    }
+    console.log('brake');
+  }
+
   return (
     <div className="App">
       <Car />
+      <button onClick={accelerate}>Accelerate</button>
+      <button onClick={brake}>Brake</button>
     </div>
   );
 }
